8f7424b58b9b7c8dc531a62ac82f4285b638da76
indirameduri <45184206+indirameduri@users.noreply.github.com>
2020-05-12 15:14:09 -0500

    python programs

---
+++ character_input.py
@@ -0,0 +1,15 @@
+'''
+Create a program that asks the user to enter their name and their age.
+Print out a message addressed to them that tells them the year that they will turn 100 years old.
+Extras:
+Add on to the previous program by asking the user for another number and printing out that many copies of the previous message. (Hint: order of operations exists in Python)
+Print out that many copies of the previous message on separate lines. (Hint: the string "\n is the same as pressing the ENTER button)'''
+import datetime 
+
+name = input('Enter your name: ')
+age = int(input('Enter your age: '))
+print()
+no = input('Enter a no below 10: ')
+d=datetime.date.today()
+for i in range(int(no)):
+    print(name.capitalize() , ' you will turn 100 in the year  : ' , (d.year - age +100))
